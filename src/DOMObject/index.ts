4845e7874c6d799d8f5c15f7a74f1a24824e59af
rakkiisto <rakkiisto@protonmail.com>
2021-09-13 16:51:17 +0900

    refactor: export types/animejs/color

--- src/DOMObject/index.ts
+++ src/DOMObject/index.ts
@@ -1,3 +1,7 @@
 import { AnimeObject } from './AnimeObject'
 
 export class DOMObject extends AnimeObject {}
+
+export * from './@Types'
+export { default as anime } from 'animejs'
+export { default as Color } from 'color'
--- src/debug.ts
+++ src/debug.ts
@@ -1,3 +1,7 @@
 import * as LuckiestDOM from '.'
 
 console.log(LuckiestDOM)
+
+import { anime, Color, CSSColorNames, mixBlendMode, _CSSStyleDeclaration } from '.'
+
+const a: anime.AnimeParams = {}
